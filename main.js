fb6e9d1341b8d15ce4cdd51e1177eeef955cbe9f
M Sukri <142651234+Msxky@users.noreply.github.com>
2024-03-12 06:54:02 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,63 @@
+const inNama = document.getElementById("inNama");
+const inUsername = document.getElementById("inUsername");
+const inKategori = document.getElementById("inKategori");
+const btn = document.getElementById("down");
+
+function getName() {
+  const inNama = document.getElementById("inNama");
+  const nama = document.getElementById("nama");
+  nama.textContent = inNama.value;
+}
+
+function getUsername() {
+  const inUsername = document.getElementById("inUsername");
+  const username = document.getElementById("username");
+  username.textContent = inUsername.value;
+}
+
+function getKategori() {
+  const inKategori = document.getElementById("inKategori");
+  const kategori = document.getElementById("kategori");
+  kategori.textContent = inKategori.value;
+}
+
+const imgDisplay = document.getElementById("imgDisplay");
+const fileInput = document.getElementById("file");
+
+fileInput.onchange = function () {
+  imgDisplay.src = URL.createObjectURL(fileInput.files[0]);
+};
+
+function download() {
+  if (inNama.value == 0 || inUsername.value == 0 || inKategori.value == 0) {
+    btn.value = "Diisi Semuanya!";
+  } else {
+    const inPrompt = prompt(
+      `halo ${inNama.value}! Isi nomor WA yg sama di group starsmusic untuk konfirmasi id ke Database`
+    );
+
+    const pesan = `Konfirmasi ID-Stars ${inNama.value} no WA wa.me/${inPrompt}`;
+    const token = "6629142888:AAGwrjziBHjKRkXyMy0i_ag3tuN4D-qgd7I";
+    const user = "1689070194";
+
+    $.ajax({
+      url: `https://api.telegram.org/bot${token}/sendMessage?chat_id=${user}&text=${pesan}&parse_mode=html`,
+      method: `POST`,
+    });
+
+    let id = document.getElementById("card");
+    id.style.transform = "scale(3)";
+    id.style.background = "var(--secondary-color)"
+    html2canvas(id).then((canvas) => {
+      id.style.background = "transparent"
+      id.style.transform = "scale(1)";
+      const url = canvas.toDataURL();
+      const a = document.createElement("a");
+      document.body.appendChild(a);
+      a.href = url;
+      a.download = `ID-Stars ${inUsername.value}.png`;
+      a.click();
+      document.body.removeChild(a);
+    });
+  }
+}
