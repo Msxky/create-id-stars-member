41d919ea3828b2d4d0f127571b6364910135d85c
M Sukri <142651234+Msxky@users.noreply.github.com>
2024-03-13 19:24:06 +0700

    Update main.js

--- main.js
+++ main.js
@@ -36,7 +36,7 @@ function download() {
       `halo ${inNama.value}! Isi nomor WA yg sama di group starsmusic untuk konfirmasi id ke Database`
     );
 
-    const pesan = `Konfirmasi ID-Stars ${inNama.value} no WA wa.me/${inPrompt}
+    const pesan = `Konfirmasi ID-Stars ${inNama.value} WhatsApp ${inPrompt}
     ${inNama.value}
     ${inUsername.value}
     ${inKategori.value}`;
