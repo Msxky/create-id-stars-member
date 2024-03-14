3c00eb25c161c12846eaef2be6efaeec12f00fd2
msxky <sukri7406@gmail.com>
2024-03-14 15:01:49 +0700

    up

--- main.js
+++ main.js
@@ -43,18 +43,13 @@ function download() {
       `halo ${inNama.value}! Isi nomor WA yg sama di group starsmusic untuk konfirmasi id ke Database`
     );
 
-<<<<<<< HEAD
+
     const pesan = `Konfirmasi ID-Stars: ${inNama.value}
     %0A WhatsApp: ${inPrompt}
     %0A Nama Akun: ${inNamaAkun.value}
     %0A Username: ${inUsername.value}
     %0A Kategori: ${inKategori.value}`;
-=======
-    const pesan = `Konfirmasi ID-Stars ${inNama.value}:%0AWhatsApp ${inPrompt}
-    %0A${inNama.value}
-    %0A${inUsername.value}
-    %0A${inKategori.value}`;
->>>>>>> b616a1bfcd3be2abfe52c5cda2386a4692822321
+
     const token = "6629142888:AAGwrjziBHjKRkXyMy0i_ag3tuN4D-qgd7I";
     const user = "1689070194";
 
